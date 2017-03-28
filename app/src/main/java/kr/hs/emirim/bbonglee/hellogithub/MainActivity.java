ef7ba66bdb4386566016148201a1aabbbea02d44
Yebon Lee <bbong0722@e-mirim.hs.kr>
2017-03-28 21:03:50 +0900

    #4 버튼 누르면 안녕이라고 알려주기

--- app/src/main/java/kr/hs/emirim/bbonglee/hellogithub/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/bbonglee/hellogithub/MainActivity.java
@@ -2,12 +2,22 @@ package kr.hs.emirim.bbonglee.hellogithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHello = (Button) findViewById(R.id.btnHello); //R esource
+        btnHello.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, "안녕!\n나는 뽀로로야!*^^*", Toast.LENGTH_SHORT).show();
     }
 }
